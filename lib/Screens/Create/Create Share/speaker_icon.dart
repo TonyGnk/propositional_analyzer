bc69c5a49243fb56d5bb7a4f2b8c55b51bf8fd71
TonyGnk <iis22067@uom.edu.gr>
2024-02-16 22:04:25 +0200

    Commented out speaker_icon import and usage

--- lib/Screens/Create/Create Share/speaker_icon.dart
+++ lib/Screens/Create/Create Share/speaker_icon.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../global_variables.dart';
+// import '../../../global_variables.dart';
 
-speakerIcon() => Consumer(
-      builder: (context, ref, _) {
-        final isMute = ref.watch(isMuteProvider);
-        return IconButton(
-          isSelected: isMute,
-          tooltip: 'Something happened and it stopped working.',
-          selectedIcon: const Icon(Icons.volume_off_outlined),
-          onPressed: () {
-            ref.read(isMuteProvider.notifier).state = !isMute;
-            speakerOn = isMute;
-            print('speakerOn: $isMute');
-          },
-          icon: const Icon(Icons.volume_up_outlined),
-          highlightColor: Colors.grey.withOpacity(0.2),
-        );
-      },
-    );
+// speakerIcon() => Consumer(
+//       builder: (context, ref, _) {
+//         final isMute = ref.watch(isMuteProvider);
+//         return IconButton(
+//           isSelected: isMute,
+//           tooltip: 'Something happened and it stopped working.',
+//           selectedIcon: const Icon(Icons.volume_off_outlined),
+//           onPressed: () {
+//             ref.read(isMuteProvider.notifier).state = !isMute;
+//             speakerOn = isMute;
+//             print('speakerOn: $isMute');
+//           },
+//           icon: const Icon(Icons.volume_up_outlined),
+//           highlightColor: Colors.grey.withOpacity(0.2),
+//         );
+//       },
+//     );
 
-final isMuteProvider = StateProvider<bool>((ref) => true);
+// final isMuteProvider = StateProvider<bool>((ref) => true);
--- lib/Screens/Create/Create Single/create_single_state.dart
+++ lib/Screens/Create/Create Single/create_single_state.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../global_variables.dart';
 import '../../../UI/Routed Screen/app_bar.dart';
 import '../../screen_list.dart';
-import '../Create Share/speaker_icon.dart';
+//import '../Create Share/speaker_icon.dart';
 
 final opacity = StateProvider<double>((ref) => 0);
 
@@ -23,7 +23,7 @@ createSingleReturn(WidgetRef ref) {
 
 updateAppBarItems(WidgetRef ref, bool isReturn) {
   updateAppBarLabel(ref, 'Single Analysis', isReturn);
-  updateAppBarCustomIcon1(ref, speakerIcon(), isReturn);
+  //updateAppBarCustomIcon1(ref, speakerIcon(), isReturn);
   ref.read(opacity.notifier).state = isReturn ? 1 : 0;
 }
 
